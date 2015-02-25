9afb32d3bbdebe36ff26851bf2f18951e9826996
ecoreng <me@ecor.me>
2015-02-25 00:02:22 -0800

    preliminary ampq adapter

--- src/Concrete/Adapters/AMPQAdapter.php
+++ src/Concrete/Adapters/AMPQAdapter.php
@@ -13,57 +13,68 @@ use \PhpAmqpLib\Message\AMQPMessage;
  */
 class AMPQAdapter implements \ecoreng\MessageBoy\Adapter
 {
-
     protected $connection;
     protected $channel;
+    protected $exchange;
+    protected $createQueues;
 
-    public function __construct(Connection $connection = null)
+    public function __construct(Connection $connection = null, $exchange = '', $createQueues = false)
     {
         if ($connection === null) {
-            // default credentials on a localhost
+            // default credentials on localhost
             $this->connection = new Connection('localhost', 5672, 'guest', 'guest');
         } else {
             $this->connection = $connection;
         }
         $this->channel = $this->connection->channel();
+        $this->exchange = $exchange;
+        $this->createQueues = $createQueues;
     }
 
     public function handle(Message $message)
     {
+        $params = $message->getParams();
+
+        $contentType = 'text/plain';
+        if (isset($params['ampq.content-type'])) {
+            $contentType = $params['ampq.content-type'];
+        }
+
+        $deliveryMode = 2;
+        if (isset($params['ampq.delivery-mode'])) {
+            $deliveryMode = $params['ampq.delivery-mode'];
+        }
+        $properties = ['content_type' => $contentType, 'delivery_mode' => $deliveryMode];
+
         // subject equals routing key
         // params equals headers
         // destinataries equal queues ?
-        
-        // make this configurable
-        $exchange = '';
-        
-        // make these configurable
-        $properties = array('content_type' => 'text/plain', 'delivery_mode' => 2);
-        
-        
         // exchanges in RabbitAMPQ
         // (default) -> automatic direct routing (key = queue name)
         // amq.direct -> routing key is used to pick a queue
         // amq.fanout -> ignores routing key and delivers to all
         // amq.topic -> messages are routed depending on routing key and pattern binding?
         // amq.match (amq.headers in rabbitmq) -> uses message headers to route instead of the key
-        
+        //
         // or any custom exchange
-        
-        // create a queue
-        // $this->channel->queue_declare($to, false, false, false, false);
-        
-        
+
+        $exchange = $this->exchange;
+        if (isset($params['ampq.exchange'])) {
+            $deliveryMode = $params['ampq.exchange'];
+        }
+
         foreach ($message->getTo() as $routingKey) {
             $msg = new AMQPMessage((string) $message->getBody(), $properties);
-            
-            // check batch publishing
-            // https://github.com/videlalvaro/php-amqplib#batch-publishing
-            $this->channel->basic_publish($msg, $exchange, $routingKey);
+
+            if ($this->createQueues) {
+                $this->channel->queue_declare($routingKey, false, false, false, false);
+            }
+
+            $this->channel->batch_basic_publish($msg, $exchange, $routingKey);
         }
-        
+        $this->channel->publish_batch();
+
         $this->channel->close();
         $this->connection->close();
     }
-
 }
